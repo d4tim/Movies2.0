43e60b69e2933ddce912a1659879e73ac16cd2fb
d4tim <seerlaken@gmail.com>
2023-12-07 20:19:14 +0200

    First_Commit_base

---
+++ Movies2.0/Movies2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies2.0
+//
+//  Created by Дмитрий Тимаров on 07.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
